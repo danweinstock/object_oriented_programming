1b7988612bafe7166273556c0fc20fe245be2c74
danweinstock <dan.weinstock@yahoo.com>
2015-01-22 14:18:44 -0500

    first launch

---
+++ rover.rb
@@ -0,0 +1,14 @@
+class Rover
+	attr_accessor :read_intruction, :move, :turn
+	def initialize (read_intruction, move, turn)
+	end
+end
+
+class x < Rover
+end
+
+class y < Rover
+end
+
+class direction < Rover
+end
