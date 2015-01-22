db930b110ae3fbdfb33ee35dc3d27d7e1a31e4f7
danweinstock <dan.weinstock@yahoo.com>
2015-01-22 16:07:57 -0500

    interesting

--- rover.rb
+++ rover.rb
@@ -1,14 +1,52 @@
 class Rover
-	attr_accessor :read_intruction, :move, :turn
-	def initialize (read_intruction, move, turn)
+	attr_accessor :x, :y, :direction
+	def initialize(x = 0, y = 0, direction = "N")
+		@x = x
+		@y = y
+		@direction = direction
 	end
-end
 
-class x < Rover
-end
+	def read_instructions(instructions)
+		instructions.each_char do |instruction|
+			if instruction == "M"
+				move
+			elsif instruction == "L" || instruction == "R"
+				rotate(instruction)
+			else "That's not a valid command!"
+		    end
+		end
+	end
 
-class y < Rover
-end
+	def move
+		case @direction
+		when "N"
+			then @y +=1
+		when "S"
+			then @y -=1
+		when "E"
+			then @x +=1
+		when "W"
+			then @x -=1			
+		end  
 
-class direction < Rover
+	end
+
+	def rotate(turn)
+	
+		if ((@direction == "N") && (turn == "L")) || ((@direction == "S") && (turn == "R"))
+			@direction = "W" 
+
+		elsif ((@direction == "S") && (turn == "L")) || ((@direction == "N") && (turn == "R"))
+			@direction = "E"
+
+		elsif ((@direction == "E") && (turn == "L")) || ((@direction == "W") && (turn == "R"))
+			@direction = "N"
+		elsif ((@direction == "W") && (turn == "L")) || ((@direction == "E") && (turn == "R"))
+			@direction = "S"
+
+		end
+	end
+	def to_s
+		"My new location is #{@x}, #{@y}, #{@direction}"
+	end
 end
